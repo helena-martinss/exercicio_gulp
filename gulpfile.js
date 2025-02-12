a5e5bba6b784e984854885d464613261f9d11b58
helena <helena257424@gmail.com>
2025-02-12 20:35:39 -0300

    Alterações

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
-import gulp from 'gulp';
-import sass from 'sass';
-import gulpSass from 'gulp-sass';
-import imagemin from 'gulp-imagemin';
-import uglify from 'gulp-uglify';
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
-export function compilaSass() {
-    return gulp.src('./source/styles/main.scss')
-        .pipe(gulp.dest('./build/styles'));
-}
-
-export function comprimeImagens() {
+function comprimeImagens() {
     return gulp.src('./source/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./build/images'))
 }
 
-export function comprimeJavaScript() {
+function comprimeJavaScript() {
     return gulp.src('./source/scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./build/scripts'))
 }
 
-function watchFiles() {
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+    .pipe(gulp.dest('./build/styles'));
+}
+
+exports.default = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
     gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
     gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
 }
-export default gulp.series(watchFiles);
